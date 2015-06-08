87fede8cc82817fdc27ff517c5372c24b9fb203e
vizlo <v.zalo.bonny@gmail.com>
2015-06-08 10:43:25 -0500

    aumento de vida para el jugador

---
+++ Proyecto/Browsing/Proyecto/MasVida.java
@@ -0,0 +1,22 @@
+import greenfoot.*;
+
+/**
+ *clase MasVida es para cuando el jugador toque la imagen de esta clase recupere
+ *parte de su vida que a ido perdiendo.
+ * 
+ * @author zarazua lopez vicente
+ * @author jacobo tovar efrain
+ * @version mayo del 2015
+ */
+public class MasVida extends Movimientos
+{
+    /**
+     * Act - do whatever the MasVida wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+        
+    }    
+}
